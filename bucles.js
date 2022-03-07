497c580d571d3cdd404806ea2f46e5adfda17cf8
ardelgad <adelgadoporras@gmail.com>
2022-03-07 10:58:10 +0100

    ejemploFunciones

--- bucles.js
+++ bucles.js
@@ -1,6 +1,5 @@
 console.log("He cargado el fichero bucles.js");
 
-//Crear una función que nos permita añadir un gato al footer
 function añadirGatoFooter() {
     let gatito = document.createElement("img");
     gatito.style.width = "50px";
@@ -9,6 +8,42 @@ function añadirGatoFooter() {
     let footer = document.querySelector(".footer");
     footer.appendChild(gatito);
 }
+
+// Crear una función que dado un color por parémtro, nos cambia el color del body a dicho color
+function cambiarColorBody(color) {
+
+    console.log("Quiero cambiar el fondo a: " + color);
+    console.log("Color del fondo antes de cambiar el color del body: " + document.body.style.backgroundColor);
+
+    // 1. Obtener el body con querySelector
+    let body = document.querySelector("body");
+
+    // document.querySelector("body").style.backgroundColor = color; <-- equivalente
+
+    // 2. Asingar a la propiedad style.backgroundColor el valor del parámetro 'color'
+    body.style.backgroundColor = color;
+
+    // color = body.style.backgroundColor;
+    // body.style = "backgroundColor: 'blue'"
+}
+
+//cambiarColorBody("blue"); // cambiar el color a azul
+cambiarColorBody("#223322"); // cambiar el color a rojo
+
+// CAmbia el texto del Hero Image al valor que le pasamos por parámetro en 'lema'
+function cambiarLema(lema) {
+
+    // heroText voy a tener un nodo HTML (objeto); ya puedo manipularlo
+    // let heroText = document.getElementsByClassName("hero");
+
+    // heroText[0].children[0].textContent = lema;
+
+    document.querySelector(".hero p").textContent = lema;
+
+}
+
+//cambiarLema("Cat lives Matter!");
+cambiarLema("Give Cat not bombs!!!!!!!!!!!!!!!!");
 
 // https://lenguajejs.com/javascript/dom/crear-elementos-dom/
 
@@ -21,24 +56,52 @@ function añadirGatoFooter() {
 // header.appendChild(despedida);
 
 // Voy a crear numGatos imagénes; y las voy añadir como hijos del footer
-/* let gatito = document.createElement("img");
-gatito.style.width = "50px";
-gatito.src = "./img/logo.webp";
 
-let footer = document.querySelector(".footer"); */
+
+/**
+ * 
+ * La función va a añadir 'numeroDeGatos' al footer
+ * 
+ * @param {string} numeroDeGatos Número de gatos a añadir
+ */
+function añadirGatos(numeroDeGatos) {
+    for (let i = 0; i < numeroDeGatos; i++) {
+        console.log("He entrado en el bucle!");
+        añadirGatoFooter();
+    }
+}
 
 // Cómo haríamos un for para ejecutar este código tantas veces como numGatos
 console.log("numGatos: ", numGatos);
-for (let i = 0; i < numGatos; i++) {
-    console.log("He entrado en el bucle!");
+añadirGatos(10) // añade 2 gatos al footer
+//añadirGatos(3) // añade 3 gatos al footer
 
-    añadirGatoFooter();
-  /*   gatito = document.createElement("img");
-    gatito.style.width = "50px";
-    gatito.src = "./img/logo.webp";
+/**
+ * Añade un borde de 'grosorBorde' píxeles y de color 'colorBorde' al DOM
+ * 
+ * @param {object} elemento Elemento del DOM 
+ * @param {number} grosorBorde Número de píxeles 
+ * @param {string} colorBorde El color en hexadecimal, o literalmente un color soportado por CSS 
+ */
+ function cambiarBordeElemento(elemento, grosorBorde, colorBorde){
+   //existe la propiedad style para cambiar el color del borde (entre otras cosa) y el grosor
+     // style.borderColor, style.borderWidth 
+    // tengo que setear style.borderStyle a "solid" siempre
+    elemento.style.border = `${grosorBorde}px ${colorBorde} solid`;
 
-    footer.appendChild(gatito); */
+   /*  elemento.style.borderColor = colorBorde;
+    elemento.style.borderWidth = grosorBorde + "px";
+    elemento.style.borderStyle = "solid"; */
 }
+
+let logo = document.querySelector(".header--logo");
+cambiarBordeElemento(logo, 5, "red"); // voy a poner un borde rojo de 5px en el logo
+
+let footer = document.querySelector(".footer");
+cambiarBordeElemento(footer, 10, "green");
+
+let body = document.querySelector("body");
+cambiarBordeElemento(body, 15, "purple");
 
 // Ejercicios Bucle y condicionales
 
